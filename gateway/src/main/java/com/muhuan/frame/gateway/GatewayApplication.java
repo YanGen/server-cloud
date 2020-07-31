b7ba71be43a3342cc0f0ff950e96f9e59a9879d4
YanGen <zorage@qq.com>
2020-07-31 17:08:48 +0800

    init

--- gateway/src/main/java/com/muhuan/frame/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/muhuan/frame/gateway/GatewayApplication.java
@@ -3,12 +3,14 @@ package com.muhuan.frame.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients(basePackages = {"com.muhuan.frame.gateway.feign"})
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.muhuan.frame.gateway"})
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
